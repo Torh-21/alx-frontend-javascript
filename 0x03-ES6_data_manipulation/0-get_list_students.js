f0bc1423d7883090c27b2948df68cd35aabba13b
Torh-21 <majarotoheeb@gmail.com>
2024-03-12 19:43:31 +0100

    ES6 DATA MANIPULATIONS Project added

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+// The function getListStudents returns an array of objects
+
+const getListStudents = () => {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+};
+
+export default getListStudents;
