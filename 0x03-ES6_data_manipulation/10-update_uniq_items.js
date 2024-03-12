052c183a7ff8fee0d34a4d20111c887f2d959bdb
Torh-21 <majarotoheeb@gmail.com>
2024-03-12 23:24:47 +0100

    More map DS task added

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,18 @@
+// The updateUniqueItems function returns an updated
+// map for all items with initial quantity at 1
+
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+};
+
+export default updateUniqueItems;
